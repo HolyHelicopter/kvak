084dc172d794eb0a1cc78cccc151cf84ad7354f3
Markunec <holyhelicopter@yandex.ru>
2020-10-09 06:33:23 +0400

    *

--- kvak/views/bot_handler.py
+++ kvak/views/bot_handler.py
@@ -8,8 +8,6 @@ def bot_handler(request):
 
     print('https://b24-tmfi1y.bitrix24.ru/rest/1/p12anzljw63jx12z/im.notify/?to=1&message=' + message)
 
-    return HttpResponse('success')
-
     resp = requests.get(
         "https://bing-image-search1.p.rapidapi.com/images/search?q=жаба",
         headers={
@@ -20,15 +18,19 @@ def bot_handler(request):
     ).json()
     # resp['value'][12]['contentUrl']
 
+    print(resp.text)
+
+    return HttpResponse('success')
+
     file_link = ''
 
     file_content = requests.get(file_link)
     file_content = file_content.content
     data = {
-        'chat_id': 'CHAT ID !!!',
+        'chat_id': '811288345',
     }
     requests.post(
-        "https://api.telegram.org/bot" + 'BOT_TOKEN !!!' + "/sendPhoto",
+        "https://api.telegram.org/bot1221959365:AAHAZKkaa5hJF0bchJFfVM9uT9Hhv-jOfzg/sendPhoto",
         data=data,
         files={'photo': ('квак.jpg', file_content)}
     )
